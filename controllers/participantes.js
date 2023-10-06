3e9f6183141c23e59ce44fcfff47ad96d4b3e603
BerriosUmbriaJD <147112965+BerriosUmbriaJD@users.noreply.github.com>
2023-10-06 12:39:29 -0400

    Update participantes.js

--- controllers/participantes.js
+++ controllers/participantes.js
@@ -2,6 +2,55 @@ const CategoriasController = require('./categorias');
 
 let participantes = [];
 
+// Método para editar un participante
+function editarParticipante(req, res) {
+  const participanteId = req.params.id;
+  const nuevaInformacion = req.body;
+
+  // Aquí deberías implementar la lógica para buscar el participante en tu sistema según el ID recibido
+  // y actualizar sus propiedades con los nuevos valores proporcionados en nuevaInformacion.
+
+  // Ejemplo de implementación:
+  const participante = obtenerParticipantePorId(participanteId);
+
+  if (participante) {
+    // Actualizar las propiedades del participante con los nuevos valores
+    participante.nombre = nuevaInformacion.nombre;
+    participante.edad = nuevaInformacion.edad;
+    participante.email = nuevaInformacion.email;
+
+    // Enviar respuesta con código de estado 200 y mensaje de éxito
+    return res.status(200).json({ mensaje: 'Participante editado exitosamente' });
+  } else {
+    // Enviar respuesta con código de estado 404 y mensaje de error si el participante no existe
+    return res.status(404).json({ mensaje: 'El participante no existe' });
+  }
+}
+
+// Función auxiliar para obtener un participante por su ID (ejemplo)
+function obtenerParticipantePorId(id) {
+  // Aquí deberías implementar la lógica para buscar y retornar el participante por su ID en tu sistema
+  // Puede ser mediante consultas a una base de datos, acceso a un arreglo de participantes, etc.
+  // Retorna el participante encontrado o null si no se encuentra
+  // Ejemplo:
+  const participantes = [
+    { id: 1, nombre: 'Participante 1', edad: 25, email: 'participante1@example.com' },
+    { id: 2, nombre: 'PartMis disculpas por la confusión anterior. Aquí tienes el código completo para las funciones de editar una categoría, un participante y un patrocinador:
+
+En el enrutador de categorías:
+
+```javascript
+const express = require('express');
+const router = express.Router();
+
+// Importar el controlador de categorías
+const categoriasController = require('../controllers/categoriasController');
+
+// Ruta para editar una categoría específica
+router.put('/categorias/:id', categoriasController.editarCategoria);
+
+module.exports = router;
+
 class ParticipantesController {
     insertar(equipo) {
         /* insertar equipo */
