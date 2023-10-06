41958a3e5eace03da584831e2e5c895403f18203
BerriosUmbriaJD <147112965+BerriosUmbriaJD@users.noreply.github.com>
2023-10-06 12:37:19 -0400

    Update categorias.js

--- controllers/categorias.js
+++ controllers/categorias.js
@@ -9,35 +9,47 @@ class CategoriasController {
         /* editar categoria */
     }
 
-    // Método para editar una categoría
+// Método para editar una categoría
 function editarCategoria(req, res) {
   const categoriaId = req.params.id;
   const nuevaInformacion = req.body;
 
-  // Aquí deberías implementar la lógica para buscar y actualizar la categoría en tu sistema
-  // Por ejemplo, las categorias, puedes utilizar una consulta para actualizar los datos
+  // Aquí deberías implementar la lógica para buscar la categoría en tu sistema según el ID recibido
+  // y actualizar sus propiedades con los nuevos valores proporcionados en nuevaInformacion.
 
-  // Ejemplo de actualización de categoría en una base de datos
-  Categoria.findByIdAndUpdate(
-    categoriaId,
-    { $set: nuevaInformacion },
-    { new: true }
-  )
-    .then(categoriaActualizada => {
-      if (categoriaActualizada) {
-        // La categoría se actualizó correctamente
-        res.status(200).json({ mensaje: 'Categoría editada exitosamente' });
-      } else {
-        // La categoría no existe
-        res.status(404).json({ mensaje: 'La categoría no existe' });
-      }
-    })
-    .catch(error => {
-      // Ocurrió un error durante la actualización
-      res.status(500).json({ mensaje: 'Error al editar la categoría' });
-    });
+  // Ejemplo de implementación:
+  const categoria = obtenerCategoriaPorId(categoriaId);
+
+  if (categoria) {
+    // Actualizar las propiedades de la categoría con los nuevos valores
+    categoria.nombre = nuevaInformacion.nombre;
+    categoria.descripcion = nuevaInformacion.descripcion;
+
+    // Enviar respuesta con código de estado 200 y mensaje de éxito
+    return res.status(200).json({ mensaje: 'Categoría editada exitosamente' });
+  } else {
+    // Enviar respuesta con código de estado 404 y mensaje de error si la categoría no existe
+    return res.status(404).json({ mensaje: 'La categoría no existe' });
+  }
 }
 
+// Función auxiliar para obtener una categoría por su ID (ejemplo)
+function obtenerCategoriaPorId(id) {
+  // Aquí deberías implementar la lógica para buscar y retornar la categoría por su ID en tu sistema
+  // Puede ser mediante consultas a una base de datos, acceso a un arreglo de categorías, etc.
+  // Retorna la categoría encontrada o null si no se encuentra
+  // Ejemplo:
+  const categorias = [
+    { id: 1, nombre: 'Categoría 1', descripcion: 'Descripción de la categoría 1' },
+    { id: 2, nombre: 'Categoría 2', descripcion: 'Descripción de la categoría 2' },
+  ];
+
+  return categorias.find(categoria => categoria.id === id);
+}
+
+module.exports = {
+  editarCategoria
+};
 module.exports = { editarCategoria };
     eliminar(idCategoria) {
         categorias.find((categoria, i) => {
