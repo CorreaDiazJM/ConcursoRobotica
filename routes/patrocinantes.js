79e0aab06c439381c4bbdb9173ef680ce3f9fd81
RicardoBRJ <147112034+RicardoBRJ@users.noreply.github.com>
2023-10-06 12:44:37 -0400

    Update patrocinantes.js

--- routes/patrocinantes.js
+++ routes/patrocinantes.js
@@ -1,10 +1,17 @@
 let express = require('express');
 let router = express.Router();
+var PatrocinantesController = require("../controllers/patrocinantes")
 
 /* GET users listing. */
 router.get('/', (req, res) => {
     res.send('Mostrar todos los patrocinantes');
 });
+
+//insertar y mostrar lista actualizada
+router.post('/', function(req, res, next) {
+  PatrocinantesController.insertar(req.body)
+  res.send(PatrocinantesController.mostrar());
+})
 
 router.get('/:id', (req, res) => {
     res.send('Mostrar patrociante ' + req.params.id);
