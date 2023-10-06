a24f26f477432a8765f0b4e20d15ca86a787c887
BerriosUmbriaJD <147112965+BerriosUmbriaJD@users.noreply.github.com>
2023-10-06 11:46:45 -0400

    Update categorias.js
    ahi les deje las cosas nuevas, solo falta adaptarlo

--- controllers/categorias.js
+++ controllers/categorias.js
@@ -9,6 +9,36 @@ class CategoriasController {
         /* editar categoria */
     }
 
+    // Método para editar una categoría
+function editarCategoria(req, res) {
+  const categoriaId = req.params.id;
+  const nuevaInformacion = req.body;
+
+  // Aquí deberías implementar la lógica para buscar y actualizar la categoría en tu sistema
+  // Por ejemplo, las categorias, puedes utilizar una consulta para actualizar los datos
+
+  // Ejemplo de actualización de categoría en una base de datos
+  Categoria.findByIdAndUpdate(
+    categoriaId,
+    { $set: nuevaInformacion },
+    { new: true }
+  )
+    .then(categoriaActualizada => {
+      if (categoriaActualizada) {
+        // La categoría se actualizó correctamente
+        res.status(200).json({ mensaje: 'Categoría editada exitosamente' });
+      } else {
+        // La categoría no existe
+        res.status(404).json({ mensaje: 'La categoría no existe' });
+      }
+    })
+    .catch(error => {
+      // Ocurrió un error durante la actualización
+      res.status(500).json({ mensaje: 'Error al editar la categoría' });
+    });
+}
+
+module.exports = { editarCategoria };
     eliminar(idCategoria) {
         categorias.find((categoria, i) => {
             if (categoria.id === idCategoria) {
